b13dab396a00675a783a9b8b901f2da9d9e65090
ivannavvaz <ivannavarrovazq@proton.me>
2024-01-16 18:11:37 +0100

    CalidadController metodo GET

--- src/Controller/CalidadController.php
+++ src/Controller/CalidadController.php
@@ -2,13 +2,31 @@
 
 namespace App\Controller;
 
+use App\Entity\Calidad;
+use App\Entity\Idioma;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Serializer\SerializerInterface;
 
 class CalidadController extends AbstractController
 {
-    public function index()
+    public function calidades(Request $request, SerializerInterface $serializer)
     {
-        return new Response('Hello!');
+        if ($request->isMethod('GET')) {
+            $calidades = $this->getDoctrine()
+                ->getRepository(Calidad::class)
+                ->findAll();
+
+            $calidades = $serializer->serialize($calidades,
+                'json',
+                ['groups' => ['calidad']]
+            );
+
+            return new Response($calidades);
+        }
+
+        return new JsonResponse(['msg' => $request->getMethod() . ' not allowed']);
     }
 }
