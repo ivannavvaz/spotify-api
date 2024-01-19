8d304f7f6d1a6b68ff5d21915615798b676f8777
Yago Calabuig <yago.calabuig@gmail.com>
2024-01-19 23:38:23 +0100

    Modificado Usuario Controller de forma definitiva para /usuario [GET,PUT]

--- src/Controller/UsuarioController.php
+++ src/Controller/UsuarioController.php
@@ -2,6 +2,10 @@
 
 namespace App\Controller;
 
+use App\Entity\Calidad;
+use App\Entity\Configuracion;
+use App\Entity\Idioma;
+use App\Entity\TipoDescarga;
 use App\Entity\Usuario;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
@@ -14,35 +18,109 @@ class UsuarioController extends AbstractController
     public function usuarios(Request $request, SerializerInterface $serializer)
     {
         if ($request->isMethod('GET')) {
-            $usuarios = $this->getDoctrine()
+            $usuario = $this->getDoctrine()
                 ->getRepository(Usuario::class)
                 ->findAll();
 
-            $usuarios = $serializer->serialize($usuarios, 'json', ['groups' => ['usuarios']]);
+            $usuario = $serializer->serialize($usuario, 'json', ['groups' => ['usuarios']]);
 
-            return new Response($usuarios);
+            return new Response($usuario);
         }
 
         if ($request->isMethod('POST')) {
 
             $data = $request->getContent();
-            $usuarios = $serializer->deserialize($data, Usuario::class, 'json');
+            $usuario = $serializer->deserialize($data, Usuario::class, 'json');
 
             $this->getDoctrine()
                 ->getManager()
-                ->persist($usuarios);
+                ->persist($usuario);
 
             $this->getDoctrine()
                 ->getManager()
                 ->flush();
 
-            $usuarios = $serializer->serialize($usuarios, 'json', ['groups' => ['usuarios']]);
 
-            return new Response($usuarios);
+
+            // crear configuracion basica para el usuario
+
+
+            $configuracion = new Configuracion();
+            $configuracion->setUsuario($usuario);
+            $configuracion->setAutoplay(false);
+            $configuracion->setAjuste(0);
+            $configuracion->setNormalizacion(false);
+            $configuracion->setIdioma($this->getDoctrine()->getRepository(Idioma::class)->findOneBy(['id' => 1]));
+            $configuracion->setCalidad($this->getDoctrine()->getRepository(Calidad::class)->findOneBy(['id' => 1]));
+            $configuracion->setTipoDescarga($this->getDoctrine()->getRepository(TipoDescarga::class)->findOneBy(['id' => 1]));
+
+            $this->getDoctrine()
+                ->getManager()
+                ->persist($configuracion);
+
+            $this->getDoctrine()
+                ->getManager()
+                ->flush();
+
+            $usuario = $serializer->serialize($usuario, 'json', ['groups' => ['usuarios']]);
+
+            return new Response($usuario);
 
         }
 
         return new JsonResponse(['msg' => $request->getMethod() . ' not allowed']);
 
     }
+
+    public function usuario(Request $request, SerializerInterface $serializer)
+    {
+
+        if ($request->isMethod('GET')) {
+            $id = $request->get('id');
+
+            $usuario = $this->getDoctrine()
+                ->getRepository(Usuario::class)
+                ->findOneBy(['id' => $id]);
+
+            $usuario = $serializer->serialize($usuario, 'json', ['groups' => ['usuarios']]);
+
+            return new Response($usuario);
+        }
+
+        if ($request->isMethod('PUT')) {
+
+            $id = $request->get('id');
+
+            $usuario = $this->getDoctrine()
+                ->getRepository(Usuario::class)
+                ->findOneBy(['id' => $id]);
+
+            if (!empty($usuario)) {
+                $data = $request->getContent();
+                $usuario = $serializer->deserialize(
+                    $data,
+                    Usuario::class,
+                    'json',
+                    ['object_to_populate' => $usuario]
+                );
+
+                $this->getDoctrine()
+                    ->getManager()
+                    ->persist($usuario);
+
+                $this->getDoctrine()
+                    ->getManager()
+                    ->flush();
+
+                $usuario = $serializer->serialize($usuario, 'json', ['groups' => ['usuarios']]);
+                return new Response($usuario);
+            }
+
+            return new JsonResponse(['msg' => 'Usuario not found']);
+        }
+
+        return new JsonResponse(['msg' => $request->getMethod() . ' not allowed']);
+
+    }
+
 }
