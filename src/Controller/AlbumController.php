9e92eff832c9bacd5db18399599b957851fb4440
ivannavvaz <ivannavarrovazq@proton.me>
2024-02-12 17:52:43 +0100

    Modificado AlbumController para ver los albums que siguen un usuario

--- src/Controller/AlbumController.php
+++ src/Controller/AlbumController.php
@@ -5,6 +5,7 @@ namespace App\Controller;
 use App\Entity\Album;
 use App\Entity\Cancion;
 use App\Entity\Idioma;
+use App\Entity\Usuario;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
@@ -73,6 +74,27 @@ class AlbumController extends AbstractController
             );
 
             return new Response($canciones);
+        }
+
+        return new JsonResponse(['msg' => $request->getMethod() . ' not allowed']);
+    }
+
+    public function albumsUsuario(Request $request, SerializerInterface $serializer)
+    {
+        $id = $request->get('usuario_id');
+
+        if ($request->isMethod('GET')) {
+            $albums = $this->getDoctrine()
+                ->getRepository(Usuario::class)
+                ->findOneBy(['id' => $id]);
+
+            $albums = $serializer->serialize(
+                $albums,
+                'json',
+                ['groups' => ['album_for_usuario', 'album_for_user', 'artista_for_album', 'artista_for_user']]
+            );
+
+            return new Response($albums);
         }
 
         return new JsonResponse(['msg' => $request->getMethod() . ' not allowed']);
